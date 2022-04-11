5d572983d6582de5fa5f599a563420ee9e405021
PiloeGAO <leonumerique@gmail.com>
2022-04-11 16:19:30 +0200

    ADD: Houdini: New SetDressing Nodes

---
+++ houdini/setDressTools/__init__.py
@@ -0,0 +1,15 @@
+from .importSetDress import ImportSetDress
+from .loadAsset import LoadAsset
+
+"""
+import sys
+
+to_del = []
+
+for module in sys.modules:
+    if("setDressTools" in module):
+        to_del.append(module)
+
+for mod in to_del:
+    del sys.modules[mod]
+"""
---
+++ houdini/setDressTools/loadAsset.py
@@ -0,0 +1,13 @@
+import os
+import hou
+
+class LoadAsset:
+    def __init__(self) -> None:
+        pass
+
+    def build_ui(self, hou_node) -> None:
+        """ Build the ui interface.
+        Args:
+            hou_node (`class` : hou.Node): the current hda node.
+        """
+        pass
